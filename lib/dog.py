742d8983f7c91c06b9cb45d18015db243fe47082
kenedy4 <kennedymusyoki8@gmail.com>
2024-08-31 12:46:12 +0300

    Added the requested classes and instances

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    bark_sound = "Woof!"
+
+    def __init__(self, name="Unnamed", breed="Mutt"):
+        self.name = name
+        self.breed = breed
+
+    def sit(self):
+        print("The dog is sitting.")
+
+    def bark(self):
+        print(self.bark_sound)
